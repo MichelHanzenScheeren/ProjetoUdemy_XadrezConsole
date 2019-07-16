a20b529c9e2d95cfd0677da3be0f6a7b1159aeb0
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2019-07-16 00:41:16 -0300

    Criada a classe Posicao

--- Projeto_JogoXadrez/Program.cs
+++ Projeto_JogoXadrez/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace Projeto_JogoXadrez
 {
@@ -10,6 +11,10 @@ namespace Projeto_JogoXadrez
     {
         static void Main(string[] args)
         {
+            Posicao posicao = new Posicao(2, 4);
+            Console.WriteLine(posicao);
+
+            Console.ReadKey();
         }
     }
 }
